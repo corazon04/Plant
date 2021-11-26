e289b22e5f2ccc1610bc51a9743e4f3a780a1858
corazon04 <faralrafizal9@gmail.com>
2021-11-26 21:43:03 +0000

    Plant

--- src/Pro/Plant/Plant.java
+++ src/Pro/Plant/Plant.java
@@ -27,73 +27,59 @@ public class Plant {
         return jumlahPupuk;
     }
 
-    public String getJumlahPupukShow(){
+    public String getJumlahPupukShow() {
         return "" + jumlahPupuk;
     }
-    public void setJumlahPupuk(int n){
+
+    public void setJumlahPupuk(int n) {
         jumlahPupuk = n;
     }
 
-    public void setStatusTumbuh(int n){
+    public void setStatusTumbuh(int n) {
         statusTumbuh = n;
     }
-    public void beriAir(){
+
+    public void beriAir() {
         jumlahAir++;
         cekKondisiTumbuh();
     }
-    public void cekKondisiTumbuh(){
-        if(jumlahAir >= 3 && jumlahPupuk >= 1){
+
+    public void cekKondisiTumbuh() {
+        if (jumlahAir >= 3 && jumlahPupuk >= 1) {
             tumbuh();
         }
     }
-    public void tumbuh(){
-        if(statusTumbuh< 4){
-            jumlahAir= jumlahAir-3;
-            jumlahPupuk= jumlahPupuk-1;
+
+    public void tumbuh() {
+        if (statusTumbuh < 4) {
+            jumlahAir = jumlahAir - 3;
+            jumlahPupuk = jumlahPupuk - 1;
             statusTumbuh++;
         }
     }
 
+    public void displayPlant() {
+        System.out.println(getStatusTumbuhText());
+        System.out.println("Jumlah Air :" + jumlahAir);
+        System.out.println("Jumlah Pupuk" + jumlahPupuk);
+    }
+
+    public String getStatusTumbuhText() {
+        switch (statusTumbuh) {
+        case 0:
+            return "Benih";
+        case 1:
+            return "Tunas";
+        case 2:
+            return "Tanaman Kecil";
+        case 3:
+            return "Tanaman Dewasa";
+        }
+        return "Berbunga";
+    }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    public 
 
 
 
